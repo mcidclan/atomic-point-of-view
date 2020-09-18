b70047f9adde082487280b0f988d543858bb54d6
mcidclan <m.cid.clan@gmail.com>
2020-09-18 20:00:52 +0200

    -Adds quaternions related functions.

--- headers/math.hpp
+++ headers/math.hpp
@@ -10,8 +10,15 @@
     #include <cmath>
     
     namespace math {
-        template<typename T>T abs(const T);
+        template<typename T> T abs(const T);
         template<typename T> T getPower(T);
-        template <typename T> Vec3<T> mulVector(Vec3<T> v, const float s);
+        template<typename T> Vec3<T> mulVector(Vec3<T>, const float);
+        // Quaternions related functions
+        Vec4<float> getNormalized4(Vec4<float>);
+        Vec4<float> getConjugate(const Vec4);
+        Vec4<float> mulQuat(const Vec4<float> qa, const Vec4<float> qb);
+        Vec4<float> getSandwichProduct(const Vec4, const Vec4);
+        Vec4<float> getOrientedQuat(const Vec2<float>);
+        Vec4<float> getReoriented(const Vec4<float>, const Vec2<float>);
     };
 #endif
--- math.cpp
+++ math.cpp
@@ -34,4 +34,44 @@ namespace math {
     }
     
     template Vec3<float> mulVector(Vec3<float>, const float);
+
+
+    // Quaternions related functions
+    Vec4<float> getNormalized4(Vec4<float> v) {
+        const float norm = sqrtf(v.x*v.x + v.y*v.y + v.z*v.z + v.w*v.w);
+        return {
+            v.x / norm, v.y / norm, v.z / norm, v.w / norm
+        }
+    }
+
+    Vec4<float> getConjugate(const Vec4 qa) {
+        return {
+            -qa.x, -qa.y, -qa.z, qa.w
+        };
+    }
+
+    Vec4<float> mulQuat(const Vec4<float> qa, const Vec4<float> qb) {
+        return {
+            qa.w * qb.x + qa.x * qb.w + qa.y * qb.z - qa.z * qb.y,
+            qa.w * qb.y + qa.y * qb.w + qa.z * qb.x - qa.x * qb.z,
+            qa.w * qb.z + qa.z * qb.w + qa.x * qb.y - qa.y * qb.x,
+            qa.w * qb.w - qa.x * qb.x - qa.y * qb.y - qa.z * qb.z
+        };
+    }
+
+    Vec4<float> getSandwichProduct(const Vec4 v, const Vec4 q) {
+        return mulQuat(mulQuat(q, v), getConjugate(q));
+    }
+
+    Vec4<float> getOrientedQuat(const Vec2<float> orientation) {
+        if(abs(y) > 1.0) {
+            y = y / abs(y);
+        }
+        y = 1.0 - y;
+        return normalize({x, y, -1.0, 0.0});
+    }
+
+    Vec4<float> getReoriented(const Vec4<float> v, const Vec2<float> orientation) {        
+        return getSandwichProduct(position, getOrientedQuat(orientation.x);
+    }
 };
